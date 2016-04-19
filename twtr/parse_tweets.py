ca67c797925561c272c68d6005a0edaac2099f8e
Tom Carrick <knyght@knyg.ht>
2016-04-19 13:42:33 +0100

    Make get_authors() less ugly.

--- twtr/parse_tweets.py
+++ twtr/parse_tweets.py
@@ -2,14 +2,13 @@ import os
 import pickle
 
 
-def get_authors(chain):
-    a = chain[0].author.screen_name
-    b = next(status.author.screen_name for status in chain[1:] if status.author.screen_name != a)
-    return a, b
-
-
 DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(DIR, 'data')
+
+
+def get_authors(chain):
+    return set(status.author.screen_name for status in chain)
+
 
 with open(os.path.join(DIR, 'selected_tweets.pickle'), 'rb') as f:
     chains = pickle.load(f)
