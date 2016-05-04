b2a6012524b01951ebc37a40e535021b590ddcd6
Tom Carrick <knyght@knyg.ht>
2016-05-04 13:56:32 +0100

    Write up results

--- run_measures.py
+++ run_measures.py
@@ -11,6 +11,7 @@ from twtr import utils
 
 
 nlp = English()
+nlp.vocab.load_vectors(os.path.join(constants.DATA_DIR, 'word2vec_twitter_model.bin'))
 
 
 with open(os.path.join(constants.DATA_DIR, 'selected_tweets.pickle'), 'rb') as f:
@@ -24,20 +25,24 @@ for chain_id, chain in parsed_chains.items():
     messages_b = [{'textblob': TextBlob(message), 'spacy': nlp(message)} for message in chain[1::2]]
     chain_scores[chain_id] = {}
 
-    lilla_scores = silla_scores = lsm_scores = wv_scores = []
+    lilla_scores = []
+    silla_scores = []
+    lsm_scores = []
+    wv_scores = []
+
     for counter, message_a in enumerate(messages_a, 1):
         for message_b in messages_b[counter:]:
-            lilla_scores.append(lla.lilla(message_a['textblob'].words, message_b['textblob'].words))
-            silla_scores.append(lla.silla(message_a['spacy'], message_b['spacy']))
-            lsm_scores.append(lsm.dyad_lsm(message_a['spacy'], message_b['spacy']))
-            wv_scores.append(message_a['spacy'].similarity(message_b['spacy']))
+            lilla_scores.append(float(lla.lilla(message_a['textblob'].words, message_b['textblob'].words)))
+            silla_scores.append(float(lla.silla(message_a['spacy'], message_b['spacy'])))
+            lsm_scores.append(float(lsm.dyad_lsm(message_a['spacy'], message_b['spacy'])))
+            wv_scores.append(float(message_a['spacy'].similarity(message_b['spacy'])))
 
     for counter, message_b in enumerate(messages_b, 1):
         for message_a in messages_a[counter:]:
-            lilla_scores.append(lla.lilla(message_a['textblob'].words, message_b['textblob'].words))
-            silla_scores.append(lla.silla(message_a['spacy'], message_b['spacy']))
-            lsm_scores.append(lsm.dyad_lsm(message_a['spacy'], message_b['spacy']))
-            wv_scores.append(message_a['spacy'].similarity(message_b['spacy']))
+            lilla_scores.append(float(lla.lilla(message_a['textblob'].words, message_b['textblob'].words)))
+            silla_scores.append(float(lla.silla(message_a['spacy'], message_b['spacy'])))
+            lsm_scores.append(float(lsm.dyad_lsm(message_a['spacy'], message_b['spacy'])))
+            wv_scores.append(float(message_a['spacy'].similarity(message_b['spacy'])))
 
     chain_scores[chain_id]['lilla'] = statistics.mean(lilla_scores)
     chain_scores[chain_id]['silla'] = statistics.mean(silla_scores)
---
+++ run_scores.py
@@ -0,0 +1,39 @@
+import os
+import pickle
+import statistics
+
+from scipy import stats
+
+from core import constants
+
+
+with open(os.path.join(constants.DATA_DIR, 'classifications.pickle'), 'rb') as f:
+    classifications = pickle.load(f)
+
+with open(os.path.join(constants.DATA_DIR, 'scores.pickle'), 'rb') as f:
+    scores = pickle.load(f)
+
+lexical_scores = classifications['means']['lexical']
+syntactic_scores = classifications['means']['syntactic']
+semantic_scores = classifications['means']['semantic']
+mean_scores = [statistics.mean(scores) for scores in zip(*[lexical_scores, syntactic_scores, semantic_scores])]
+
+lilla_scores = [score['lilla'] for score in scores.values()]
+silla_scores = [score['silla'] for score in scores.values()]
+wv_scores = [score['wv'] for score in scores.values()]
+lsm_scores = [score['lsm'] for score in scores.values()]
+
+r, p = stats.pearsonr(lexical_scores, lilla_scores)
+print('Lexical vs LILLA: r={r}, p={p:.20f}'.format(r=r, p=p))
+
+r, p = stats.pearsonr(syntactic_scores, silla_scores)
+print('Syntactic vs SILLA: r={r}, p={p:.20f}'.format(r=r, p=p))
+
+r, p = stats.pearsonr(semantic_scores, wv_scores)
+print('Semantic vs Word2vec: r={r}, p={p:.20f}'.format(r=r, p=p))
+
+r, p = stats.pearsonr(mean_scores, lsm_scores)
+print('Mean vs LSM: r={r}, p={p:.20f}'.format(r=r, p=p))
+
+r, p = stats.pearsonr(lexical_scores, lsm_scores)
+print('Lexical vs LSM: r={r}, p={p:.20f}'.format(r=r, p=p))
