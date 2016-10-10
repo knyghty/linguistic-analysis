e43f3d2afc7857c557219b5059a11895bab56c3b
Tom Carrick <knyght@knyg.ht>
2016-10-10 15:33:37 +0100

    Rejiggy

--- measures/deps.py
+++
@@ -1,7 +0,0 @@
-def subtrees(doc):
-    deps = []
-    for word in doc:
-        children = list(word.children)
-        if len(children):
-            deps.append((word.dep, [child.dep for child in children]))
-    return deps
